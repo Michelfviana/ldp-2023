6c509bac924c9c13313fc10c23b3f562de169335
michelfviana <michel.fvc@hotmail.com>
2023-08-30 18:23:49 -0300

    linguagem C

---
+++ crescente.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+
+    printf("Digite dois numeros:\n");
+    scanf("%d,", &x);
+    scanf("%d,", &y);
+
+    while (x != y) {
+        if (x < y) {
+            printf("CRESCENTE\n");
+        }
+        else {
+            printf("Decrescente\n");
+        }
+
+        printf("Digite outros dois numeros:\n");
+        scanf("%d,", &x);
+        scanf("%d,", &y);
+    }
+    return 0;
+}
